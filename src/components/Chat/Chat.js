698432b772ad129f0a1ea9e498a24ee31966da9c
Your Name <hr5359@srmist.edu.in>
2021-03-28 12:17:26 +0530

    Chat components added

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React from "react";
+import './Chat.css';
+import Avatar from "@material-ui/core/Avatar";
+import { Link } from "react-router-dom";
 
-export class Chat extends Component {
-    render() {
-        return (
-            <div>
-                Chat Page
-            </div>
-        )
-    }
-}
+const Chat = ({ name, message, profilePic, timestamp }) => {
+  return (
+    <Link to={`/chat/${name}`}>
+    <div className="chat">
+      <Avatar className="chat_image" alt={name} src={profilePic} />
+      <div className="chat_details">
+        <h2>{name}</h2>
+        <p>{message}</p>
+      </div>
+      <p className="chat_timestamp">{timestamp}</p>
+    </div>
+    </Link>
+  );
+};
 
-export default Chat
+export default Chat;
